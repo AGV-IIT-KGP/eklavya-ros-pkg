257efcc2b770c3f2e93029cf1c74419987d39dd1
Samuel Anudeep <banmido69@gmail.com>
2013-04-03 23:09:01 +0530

    Improved merge algorithm to decrease the waiting times of other threads by decentralizing the mutex locks.

--- eklavya_node/src/Modules/Fusion/fusion.h
+++ eklavya_node/src/Modules/Fusion/fusion.h
@@ -2,6 +2,9 @@
 
 using namespace std;
 
+extern unsigned char my_lidar_map[MAP_MAX][MAP_MAX];
+extern unsigned char my_camera_map[MAP_MAX][MAP_MAX];
+
 class Fusion {
 public:
     void laneLidar();
--- eklavya_node/src/Modules/Fusion/fusion_thread.cpp
+++ eklavya_node/src/Modules/Fusion/fusion_thread.cpp
@@ -1,13 +1,19 @@
 #include "fusion.h"
 
-void *fusion_thread(void *arg) {
-  ros::Rate loop_rate(LOOP_RATE);
-  while(ros::ok()) {
-      Fusion fuse;
-      fuse.laneLidar();
-    loop_rate.sleep();
-  }
+unsigned char my_lidar_map[MAP_MAX][MAP_MAX];
+unsigned char my_camera_map[MAP_MAX][MAP_MAX];
 
-  ROS_INFO("Fusion module exiting");
-  return NULL;
+void *fusion_thread(void *arg) {
+    ros::Rate loop_rate(LOOP_RATE);
+    
+    ROS_INFO("Fusion module started");
+    
+    while (ros::ok()) {
+        Fusion fuse;
+        fuse.laneLidar();
+        loop_rate.sleep();
+    }
+
+    ROS_INFO("Fusion module exiting");
+    return NULL;
 }
