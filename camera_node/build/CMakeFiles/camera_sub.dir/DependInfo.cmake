8813578bdd87cbeed490dd4ecc95f307219225c7
shiwangi <shiwangi@shiwangi-N53SM.(none)>
2013-04-03 22:12:21 +0530

    added camera pub

---
+++ camera_node/build/CMakeFiles/camera_sub.dir/DependInfo.cmake
@@ -0,0 +1,13 @@
+# The set of languages for which implicit dependencies are needed:
+SET(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+SET(CMAKE_DEPENDS_CHECK_CXX
+  "/home/shiwangi/fuerte_workspace/sandbox/eklavya-ros-pkg/camera_node/src/camera_sub.cpp" "/home/shiwangi/fuerte_workspace/sandbox/eklavya-ros-pkg/camera_node/build/CMakeFiles/camera_sub.dir/src/camera_sub.o"
+  )
+SET(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Targets to which this target links.
+SET(CMAKE_TARGET_LINKED_INFO_FILES
+  )
